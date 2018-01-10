983c45f30092388a4c409f6e036ad140e5ae3383
Claus Wiingreen <cwiing13@student.aau.dk>
2018-01-10 12:48:22 +0100

    Removed unused data and some debug tools, and made the sidebar auto update every 3 seconds.

--- script.js
+++ script.js
@@ -1,57 +1,64 @@
-window.onload = function(){
+window.onload = function() {
+    update_sidebar();
+    setInterval(update_sidebar, 3000);
+}
+
+function update_sidebar() {
     document.getElementById("day").innerHTML = getWeekday(new Date(), "dk");
+    clear_plan();
     const day_events = events.filter(event_today);
     day_events.forEach(add_to_plan);
+}
+
+function getWeekday(date, locale = "en") {
+    const days = {
+        dk:["Søndag","Mandag","Tirsdag","Onsdag","Torsdag","Fredag","Lørdag"],
+        en:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    };
     
-    function getWeekday(date, locale){
-        const days = {
-            dk:["Søndag","Mandag","Tirsdag","Onsdag","Torsdag","Fredag","Lørdag"],
-            en:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-        };
-        
-        locale = locale ? locale : "en";
-        
-        return days[locale][date.getDay()];
-    }
+    return days[locale][date.getDay()];
+}
+
+function clear_plan() {
+    document.getElementById("plan").innerHTML = "";
+}
+
+function add_to_plan(event){    
+    const hour_minutes_format = {hour: '2-digit', minute:'2-digit'};
     
-    function add_to_plan(event){
-        const plan = document.getElementById("plan");
-        
-        if(!plan) return;
-        
-        const hour_minutes_format = {hour: '2-digit', minute:'2-digit'};
-        
-        const table_row = document.createElement("tr");
-        table_row.id = "event" + event.id;
-        
-        const row_time = document.createElement("td");
-        const row_title = document.createElement("td");
-        row_time.innerHTML = event.start.toLocaleTimeString([], hour_minutes_format);
-        row_title.innerHTML = event.title;
-        
-        if(event_is_now(event)){
-            table_row.classList.add("active_event");
-        }
-        
-        table_row.appendChild(row_time);
-        table_row.appendChild(row_title);
-        plan.appendChild(table_row);
-    }
+    const row_time = document.createElement("td");
+    row_time.innerHTML = event.start.toLocaleTimeString([], hour_minutes_format);
+
+    const row_title = document.createElement("td");
+    row_title.innerHTML = event.title;
     
-    function event_is_now(event){
-        const now = Date.now();
-        
-        return now > event.start && now < event.end;
+    const table_row = document.createElement("tr");
+    table_row.id = "event" + event.id;
+    if(event_is_now(event)){
+        table_row.classList.add("active_event");
     }
+    table_row.appendChild(row_time);
+    table_row.appendChild(row_title);
     
-    function event_today(event){
-        const today = new Date();
-        today.setDate(13);
-        today.setHours(0,0,0,0);
-        
-        const tomorrow = new Date(today.getTime()); // Ensures that the date is copied
-        tomorrow.setDate(tomorrow.getDate() + 1);
-        
-        return event.start > today && event.start < tomorrow;
-    }
+    document.getElementById("plan").appendChild(table_row);
+}
+
+function is_between(value, low, high) {
+    if(low > high) return between(value, high, low);
+    return value > low && value < high;
+}
+
+function event_is_now(event) {
+    const now = new Date();
+    return is_between(now, event.start, event.end);
+}
+
+function event_today(event) {
+    const today = new Date();
+    today.setHours(0,0,0,0);
+    
+    const tomorrow = new Date(today.getTime()); // Ensures that the date is copied
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    
+    return is_between(event.start, today, tomorrow);
 }
