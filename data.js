78ada538529ba8fc55980537b0737aa5792cf8b2
Claus Wiingreen <cwiing13@student.aau.dk>
2018-01-21 00:31:36 +0100

    Did a lot of work * Updated events to use a common class * Events now also uses a better way to define when they start * Reworked the timer system * Added timing events on page load * Moved 'event_is_now' and 'event_is_today' to the event class

--- data.js
+++ data.js
@@ -16,118 +16,116 @@ const flan_crew = {
     }
 }
 
-const events = [
-    {
-        id: 0,
-        start: new Date("2018-01-12T17:00"),
-        end: new Date("2018-01-12T18:00"),
-        title: "Dørene Åbner"
-    },
-    {
-        id: 1,
-        start: new Date("2018-01-12T18:45"),
-        end: new Date("2018-01-12T19:00"),
-        title: "Aftensmad Serveres"
-    },
-    {
-        id: 2,
-        start: new Date("2018-01-12T19:00"),
-        end: new Date("2018-01-12T20:00"),
-        title: "Velkomst og Sponsoroplæg"
-    },
-    {
-        id: 3,
-        start: new Date("2018-01-12T20:00"),
-        end: new Date("2018-01-14T10:00"),
-        title: "Heroes 3<br/>(Kører hele Weekenden)",
-        sponsor: sponsors["sponsor1"],
-        responsible: [
-            flan_crew["wiingreen"]
-        ],
-        fiki: "http://fklub.dk/flan/heroes3"
-    },
-    {
-        id: 4,
-        start: new Date("2018-01-12T20:00"),
-        end: new Date("2018-01-12T22:00"),
-        title: "Worms Amageddon",
-        sponsor: sponsors["sponsor1"],
-        responsible: [
-            flan_crew["wiingreen"]
-        ],
-        fiki: "http://fklub.dk/flan/worms",
-        signup: "[challonge link]"
-    },
-    {
-        id: 5,
-        start: new Date("2018-01-12T22:00"),
-        end: new Date("2018-01-12T00:00"),
-        title: "Overwatch 3v3",
-        sponsor: sponsors["sponsor1"],
-        responsible: [
-            flan_crew["wiingreen"]
-        ],
-        fiki: "http://fklub.dk/flan/overwatch",
-        signup: "[challonge link]"
-    },
-    {
-        id: 6,
-        start: new Date("2018-01-13T09:00"),
-        end: new Date("2018-01-13T10:00"),
-        title: "Morgenmad serveres"
-    },
-    {
-        id: 7,
-        start: new Date("2018-01-13T10:00"),
-        end: new Date("2018-01-13T12:00"),
-        title: "Warcraft 3 - Custom Maps",
-        sponsor: sponsors["sponsor1"],
-        responsible: [
-            flan_crew["wiingreen"]
-        ],
-        fiki: "http://fklub.dk/flan/warcraft3",
-        signup: "[challonge link]"
-    },
-    {
-        id: 8,
-        start: new Date("2018-01-13T12:00"),
-        end: new Date("2018-01-13T13:00"),
-        title: "Rocket League",
-        sponsor: sponsors["sponsor1"],
-        responsible: [
-            flan_crew["wiingreen"]
-        ],
-        fiki: "http://fklub.dk/flan/rocket_league",
-        signup: "[challonge link]"
-    },
-    {
-        id: 9,
-        start: new Date("2018-01-13T13:00"),
-        end: new Date("2018-01-13T14:00"),
-        title: "Sandwiches Serveres"
-    },
-    {
-        id: 10,
-        start: new Date("2018-01-13T14:00"),
-        end: new Date("2018-01-13T20:00"),
-        title: "Leagues of Legends",
-        sponsor: sponsors["sponsor1"],
-        responsible: [
-            flan_crew["wiingreen"]
-        ],
-        fiki: "http://fklub.dk/flan/lol",
-        signup: "[challonge link]"
-    },
-    {
-        id: 10,
-        start: new Date("2018-01-13T20:00"),
-        end: new Date("2018-01-14T00:00"),
-        title: "Counter Strike: Global Offence",
-        sponsor: sponsors["sponsor1"],
-        responsible: [
-            flan_crew["wiingreen"]
-        ],
-        fiki: "http://fklub.dk/flan/csgo",
-        signup: "[challonge link]"
+let next_event_id = 0;
+
+function is_between(value, low, high) {
+    if(low > high) return is_between(value, high, low);
+    return value > low && value < high;
+}
+
+class Event{
+    constructor(title, start, end, sponsor = undefined, responsible = undefined, message = undefined){
+        this.id = next_event_id++;
+        this.start = start;
+        this.end = end;
+        this.title = title;
+        this.sponsor = sponsor;
+        this.responsible = responsible;
+        this.message = message;
     }
+    
+    is_now() {
+        const now = new Date();
+        return is_between(now, this.start, this.end);
+    }
+    
+    is_today(){
+        const today = new Date();
+        today.setHours(0,0,0,0);
+        
+        const tomorrow = new Date(today.getTime()); // Ensures that the date is copied
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        
+        return is_between(this.start, today, tomorrow);
+    }
+}
+
+const day1 = "2018-01-19T"
+const day2 = "2018-01-20T"
+const day3 = "2018-01-21T"
+
+const events = [
+// Tournements
+    new Event(
+        "Heroes 3", 
+        new Date(day1 + "22:29"), 
+        new Date(day3 + "10:00"), 
+        sponsors["sponsor1"], 
+        [flan_crew["wiingreen"]]),
+    new Event(
+        "Worms Amageddon",
+        new Date(day1 + "20:00"),
+        new Date(day1 + "22:00"),
+        sponsors["sponsor1"],
+        [flan_crew["wiingreen"]]),
+    new Event(
+        "Overwatch 3v3",
+        new Date(day1 + "22:00"),
+        new Date(day2 + "00:00"),
+        sponsors["sponsor1"],
+        [flan_crew["wiingreen"]]),
+    new Event(
+        "Warcraft 3 - Custom Maps",
+        new Date(day2 + "14:50"),
+        new Date(day2 + "15:00"),
+        sponsors["sponsor1"],
+        [flan_crew["wiingreen"]]),
+    new Event(
+        "Rocket League",
+        new Date(day2 + "12:00"),
+        new Date(day2 + "13:00"),
+        sponsors["sponsor1"],   
+        [flan_crew["wiingreen"]]),
+    new Event(
+        "Leagues of Legends",
+        new Date(day2 + "14:00"),
+        new Date(day2 + "20:00"),
+        sponsors["sponsor1"],
+        [flan_crew["wiingreen"]]),
+    new Event(
+        "Counter Strike: Global Offence",
+        new Date(day2 + "20:00"),
+        new Date(day3 + "00:00"),
+        sponsors["sponsor1"],
+        [flan_crew["wiingreen"]]),
+        
+// Standard events
+    new Event(
+        "Dørene Åbner", 
+        new Date(day1 + "17:00"), 
+        new Date(day1 + "18:00")),
+    new Event(
+        "Aftensmad Servers", 
+        new Date(day1 + "18:45"), 
+        new Date(day1+ "19:00")),
+    new Event(
+        "Velkomst og Sponsoroplæg", 
+        new Date(day1 + "19:00"), 
+        new Date(day1 + "20:00")),
+    new Event(
+        "Morgenmad Serveres",
+        new Date(day2 + "09:00"),
+        new Date(day2 + "10:00")),
+    new Event(
+        "Sandwiches Serveres",
+        new Date(day2 + "13:00"),
+        new Date(day2 + "14:00")),
+    new Event(
+        "Morgenmad Serveres",
+        new Date(day3 + "09:00"),
+        new Date(day3 + "10:00")),
+    new Event(
+        "Dørene Lukkes",
+        new Date(day3 + "10:00"),
+        new Date(day3 + "11:00"))
 ];
--- script.js
+++ script.js
@@ -1,8 +1,56 @@
-window.onload = function() {
-    update_sidebar();
-    setInterval(update_sidebar, 3000);
-    setInterval(update_sponsor_logo, 3000);
+const second = 1000;
+const minute = 60*second;
+const hour = 60*minute
+
+// Setup events
+events.sort(function(a, b){
+    return a.start - b.start;
+});
+
+let now = Date.now();
+for(index in events){
+    let event = events[index];
+    let time_to_event = event.start - now;
+    
+    if(time_to_event < 0){
+        console.log("Event " + event.id + " start time has already passed.\n"
+                  + "Start time: " + event.start + "\n"
+                  + "Event title: '" + event.title + "'");
+        continue;
+    }
+    else if(event.is_now()){
+        runEvent(event);
+    }
+    if(event.sponsor){        
+        setTimeout(function(){startEventCountDown(event);}, time_to_event - 1*minute);
+    }
+    
+    console.log("Scheduled event for " + event.start + "\n" 
+              + "Milliseconds to event " + time_to_event + "\n"
+              + "Event title: '" + event.title + "'");
+    setTimeout(function(){runEvent(event);}, time_to_event);
 }
+
+window.onload = function() {
+    build_sidebar();
+    const t = new Timer(30*minute);
+    t.start();
+    setTimeout(function(){t.stop();}, 30*minute)
+}
+
+function runEvent(event){
+    console.log("Event " + event.id + " Started.");
+    const element = document.getElementById("event" + event.id);
+    element.classList.add("active_event");
+    setTimeout(function(){
+        element.classList.remove("active_event");
+        console.log("Event " + event.id + " Stopped.");
+    }, event.end - Date.now());
+}
+
+function startEventCountDown(event){
+    const t = new Timer(event.start - Date.now());
+    t.start();}
 
 const sponsor_iterator = infinite_sponsor_iterator();
 
@@ -20,11 +68,14 @@ function update_sponsor_logo(){
     sponsor_logo.style.backgroundImage = "url(" + sponsor.logo + ")";
 }
 
-function update_sidebar() {
-    document.getElementById("day").innerHTML = getWeekday(new Date(), "dk");
+function build_sidebar() {
     clear_plan();
-    const day_events = events.filter(event_today);
-    day_events.forEach(add_to_plan);
+    const header = document.createElement("h2");
+    header.innerHTML = getWeekday(new Date(), "dk");
+    document.getElementById("plan").appendChild(header);
+    events.filter(function(event){
+        return event.is_today();
+    }).forEach(add_to_plan);
 }
 
 function getWeekday(date, locale = "en") {
@@ -40,9 +91,9 @@ function clear_plan() {
     document.getElementById("plan").innerHTML = "";
 }
 
-function add_to_plan(event){    
-    const hour_minutes_format = {hour: '2-digit', minute:'2-digit'};
-    
+const hour_minutes_format = {hour: '2-digit', minute:'2-digit'};
+
+function add_to_plan(event){
     const row_time = document.createElement("td");
     row_time.innerHTML = event.start.toLocaleTimeString([], hour_minutes_format);
 
@@ -51,31 +102,76 @@ function add_to_plan(event){
     
     const table_row = document.createElement("tr");
     table_row.id = "event" + event.id;
-    if(event_is_now(event)){
-        table_row.classList.add("active_event");
-    }
+
     table_row.appendChild(row_time);
     table_row.appendChild(row_title);
     
     document.getElementById("plan").appendChild(table_row);
 }
 
-function is_between(value, low, high) {
-    if(low > high) return between(value, high, low);
-    return value > low && value < high;
-}
-
-function event_is_now(event) {
-    const now = new Date();
-    return is_between(now, event.start, event.end);
-}
-
-function event_today(event) {
-    const today = new Date();
-    today.setHours(0,0,0,0);
+function Timer(duration){
+    console.log("Timer::constructor called with parameter duration: " + duration);
+    const canvas = document.getElementById("timer");
+    const ctx = canvas.getContext("2d");
+    let radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius *= 0.90;
+    let end_time = undefined;
     
-    const tomorrow = new Date(today.getTime()); // Ensures that the date is copied
-    tomorrow.setDate(tomorrow.getDate() + 1);
+    const drawFace = function(ctx, radius) {
+      //console.log("Timer::drawFace called");
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, 2*Math.PI);
+      ctx.fillStyle = 'white';
+      ctx.fill();
+      let grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+      grad.addColorStop(0, '#333');
+      grad.addColorStop(0.5, 'white');
+      grad.addColorStop(1, '#333');
+      ctx.strokeStyle = grad;
+      ctx.lineWidth = radius*0.1;
+      ctx.stroke();
+    }
+
+    const drawTime = function(ctx, radius, time_left, duration){   
+        //console.log("Timer::drawTime called");
+        ctx.beginPath();
+        ctx.moveTo(0,0);
+        ctx.arc(0, 0, radius*0.95, -0.5*Math.PI, -0.5*Math.PI+2*Math.PI*(time_left/duration));
+        ctx.lineTo(0,0);
+        ctx.fillStyle = '#3c3';
+        ctx.fill();
+    }
+
+    const drawCountDown = function(ctx, radius, time_left){
+        //onsole.log("Timer::drawCountDown called");
+        ctx.textBaseline="middle";
+        ctx.textAlign="center";
+        ctx.font = radius*0.5 + "px arial";
+        ctx.fillStyle= '#333';
+        ctx.fillText(time_left.toLocaleTimeString([],{minute: "2-digit", second: "2-digit"}), 0, 0);
+    }
     
-    return is_between(event.start, today, tomorrow);
+    const drawClock = function() {
+      //console.log("Timer::drawClock called");
+      drawFace(ctx, radius);
+      const time_left = new Date(end_time - Date.now());
+      drawTime(ctx, radius, time_left, duration);
+      drawCountDown(ctx, radius, time_left);
+      
+      if(time_left.getMinutes() + time_left.getSeconds() == 0){
+        this.stop();
+      }
+    }
+    
+    this.start = function(){
+        console.log("Timer::start called");
+        end_time = Date.now() + duration;
+        this.interval_id = setInterval(drawClock, 1000);
+    }
+    
+    this.stop = function(){
+        console.log("Timer::stop called");
+        clearInterval(this.interval_id);
+    }
 }
